41ef8d8cf1d495c13f0ead77b3975fd341c1dd7e
techup2024-lg6-cc <techup2024.lg6@gmail.com>
2024-05-15 22:21:05 +0800

    Update script.js add calculation table

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ const totalBudget = document.getElementById("total-budget");
 const addExpenseButton = document.getElementById("expense-button");
 const nameOfItem = document.getElementById("expense-item");
 const expenseAmountOfItem = document.getElementById("expense-amount");
-const remainingBalance = document.getElementById("remaining-balance");
+
+const totalBudgetOutput = document.getElementById("total-budget-output");
+const totalExpensesOutput = document.getElementById("total-expenses-output");
+const totalBalanceOutput = document.getElementById("balance-output");
 
 setBudgetButton.addEventListener('click', function() {
     
     const tempBudgetAmount = Number(totalBudget.value);
     
-    if (isNaN(tempBudgetAmount) || tempBudgetAmount <= 0) {
+    if (isNaN(tempBudgetAmount) || tempBudgetAmount < 0) {
         alert("Please enter a valid amount.");
         return;
     }
+
+    totalBudgetOutput.innerHTML = tempBudgetAmount;
+    totalBalanceOutput.innerText = tempBudgetAmount - totalExpensesOutput.innerText;
+    totalBudget.value = "";
 });
 
 addExpenseButton.addEventListener('click', function() {
@@ -21,7 +28,7 @@ addExpenseButton.addEventListener('click', function() {
     const tempAmountOfItem = Number(expenseAmountOfItem.value);
     const tempNameOfItem = nameOfItem.value;
     
-    if (isNaN(tempAmountOfItem) || tempAmountOfItem <= 0) {
+    if (isNaN(tempAmountOfItem) || tempAmountOfItem < 0) {
         alert("Please enter a valid amount.");
         return;
     }
@@ -29,4 +36,15 @@ addExpenseButton.addEventListener('click', function() {
     if (tempNameOfItem === "") {
         alert("Please key in name of item.")
     }
+
+    let expenditure = parseInt(expenseAmountOfItem.value);
+    let sum = parseInt(totalExpensesOutput.innerText) + expenditure;
+
+    totalExpensesOutput.innerText = sum;
+
+    const remainingBalance = Number(totalBudgetOutput.innerText) - Number(totalExpensesOutput.innerText);
+    totalBalanceOutput.innerText = remainingBalance;
+
+    nameOfItem.value = "";
+    expenseAmountOfItem.value = "";
 });
